d8a1fb166ad243806a0df6ec1e63a22812f663e9
Gor G <99032002+mySpecialUsername@users.noreply.github.com>
2022-03-16 00:07:38 +0100

    Update demonstration.py

--- demonstration.py
+++ demonstration.py
@@ -12,21 +12,21 @@ from pywt2DDisplay import adapt_coeffs
 
 
 def gimshow(I, title = '', limits = None, show=True):
-  """prepares a picture to show with a colorbar, plots if show is True"""
-  if limits!=None:
-    assert(type(limits)==tuple and len(limits)==2 ), "limits (tird positional arguments) must be a tuple of len 2"
-    plt.imshow(I, cmap="gray", vmin =limits[0], vmax=limits[1])
-  else:
-    plt.imshow(I, cmap="gray")
-  plt.colorbar()
-  plt.title(title)
-  if show:
-    plt.show()
- 
-  return 0
+	"""prepares a picture to show with a colorbar, plots if show is True"""
+	if limits!=None:
+		assert(type(limits)==tuple and len(limits)==2 ), "limits (tird positional arguments) must be a tuple of len 2"
+		plt.imshow(I, cmap="gray", vmin =limits[0], vmax=limits[1])
+	else:
+		plt.imshow(I, cmap="gray")
+	plt.colorbar()
+	plt.title(title)
+	if show:
+		plt.show()
+	
+	return 0
 
 
-y = mpimg.imread("lena.png")
+y = mpimg.imread("lena.png")[:,:,0]
 w = pywt.Wavelet('db2')
 coeffs = pywt.wavedec2(y, w, level=3, mode='periodization')
 
