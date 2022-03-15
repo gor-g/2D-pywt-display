0bad87c43ff34b90c54ba7ff111c2a9f105cbe8f
Gor G <99032002+mySpecialUsername@users.noreply.github.com>
2022-03-15 23:45:21 +0100

    Update pywt2DDisplay.py

--- pywt2DDisplay.py
+++ pywt2DDisplay.py
@@ -1,47 +1,44 @@
-# Author : Gor G.
-# AKA mySpecialUsername
-# March 2022
 
 import numpy as np
 
 def adapt_coeffs(coeffs, equa=False, Hsize=1000):
-  """ EN: normalise, or normalise and applys a historgam equalisation for each coefficeint matrix of the pywt.wavedec2() output
-  FR: normalise ou normalise puis égalise l'histogramme, de chaque coefficeint du résulatat de pywt.wavedec2() """
-  def normalise(I):"""linearly normalise the input np.ndarray"""
-    I = I-np.min(I); I = I/np.max(I)
-    return I
-  
-  def equalise(I, Hsize=Hsize):
-    """
-    input: 2D np.ndarray
-    output: equalise the histogram of the input array
-    """
-    # EN: remaps the values of I to integers between 0 and Hsize to apply bitcount
-    # FR : remène les valeurs de I à des entiers entre 0 et Hsize pour appliquer bitcount
-    I = normalise(I)*(Hsize-1); I=I.astype(int) 
-    
-    # EN: calculates the normalised histogram, than the cumulated histogramm
-    # FR: calculer l'histo normalisé, puis le histo cumulé
-    H = np.bincount(I.flatten(), minlength=Hsize)
-    H = H/np.sum(H)
-    
-    cumulH = np.cumsum(H)
-    # EN: reassigns the equalised values of the pixels
-    # FR: assinge les valeurs équalisées
-    res = [cumulH[i] for i in I.flatten()]
-    return np.reshape(res, I.shape)
-  
+""" EN: normalise, or normalise and applys a historgam equalisation for each coefficeint matrix of the pywt.wavedec2() output
+FR: normalise ou normalise puis égalise l'histogramme, de chaque coefficeint du résulatat de pywt.wavedec2() """
+def normalise(I):"""linearly normalise the input np.ndarray"""
+	I = I-np.min(I); I = I/np.max(I)
+	return I
 
-  
-  # EN: coose the transformation to apply
-  # FR: choisit la transformation qu'il faut appliquer
-  tfo = equalise if equa else normalise 
-  
-  # EN: applys the transformation to each coefficient matrix
-  # FR: applique la transformation à chaque coefficient
-  res = [tfo(coeffs[0])]
-  for i in range (1,len(coeffs)):
-    T=tuple([tfo(coeffs[i][j]) for j in range(3)])
-    res+=[T]
+def equalise(I, Hsize=Hsize):
+	"""
+	input: 2D np.ndarray
+	output: equalise the histogram of the input array
+	"""
+	# EN: remaps the values of I to integers between 0 and Hsize to apply bitcount
+	# FR : remène les valeurs de I à des entiers entre 0 et Hsize pour appliquer bitcount
+	I = normalise(I)*(Hsize-1); I=I.astype(int) 
+	
+	# EN: calculates the normalised histogram, than the cumulated histogramm
+	# FR: calculer l'histo normalisé, puis le histo cumulé
+	H = np.bincount(I.flatten(), minlength=Hsize)
+	H = H/np.sum(H)
+	
+	cumulH = np.cumsum(H)
+	# EN: reassigns the equalised values of the pixels
+	# FR: assinge les valeurs équalisées
+	res = [cumulH[i] for i in I.flatten()]
+	return np.reshape(res, I.shape)
 
-  return res
+
+
+# EN: coose the transformation to apply
+# FR: choisit la transformation qu'il faut appliquer
+tfo = equalise if equa else normalise 
+
+# EN: applys the transformation to each coefficient matrix
+# FR: applique la transformation à chaque coefficient
+res = [tfo(coeffs[0])]
+for i in range (1,len(coeffs)):
+	T=tuple([tfo(coeffs[i][j]) for j in range(3)])
+	res+=[T]
+
+return res
